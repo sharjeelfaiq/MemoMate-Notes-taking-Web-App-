8283db0f353fc9e461053ec0c96201274cc1d15b
Sharjeel Faiq <mrmalik610@gmail.com>
2023-02-08 23:34:22 +0500

    Completed notes title addition by adding the functionality to it. Also added a css file and added a code to it to prevent the relocation of the website when the scroll bar appears.

--- app.js
+++ app.js
@@ -3,36 +3,50 @@ showNotes();
 // Function to store user input to the localStorage
 const addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", () => {
+  let addTitle = document.getElementById("addTitle");
   let addTxt = document.getElementById("addTxt");
+  let titlesStr = localStorage.getItem("titles");
   let notesStr = localStorage.getItem("notes");
+  let titlesObj;
   let notesObj;
-  if (notesStr != null) {
-    notesObj = JSON.parse(notesStr);
-  } else {
+  if (notesStr == null && titlesStr == null) {
+    titlesObj = [];
     notesObj = [];
+  } else {
+    titlesObj = JSON.parse(titlesStr);
+    notesObj = JSON.parse(notesStr);
   }
+  titlesObj.push(addTitle.value);
   notesObj.push(addTxt.value);
+  localStorage.setItem("titles", JSON.stringify(titlesObj));
   localStorage.setItem("notes", JSON.stringify(notesObj));
   // Calling showNotes() fucntion defined ahead.
   showNotes();
+  addTitle.value = "";
   addTxt.value = "";
 });
 
 // Function to retrive and show the elements stored in the localStorage
 function showNotes() {
+  let titlesStr = localStorage.getItem("titles");
   let notesStr = localStorage.getItem("notes");
+  let titlesObj;
   let notesObj;
-  if (notesStr == null) {
+  if (notesStr == null && titlesStr == null) {
+    titlesObj = [];
     notesObj = [];
   } else {
+    titlesObj = JSON.parse(titlesStr);
     notesObj = JSON.parse(notesStr);
   }
+  let title = "";
   let html = "";
   notesObj.forEach((element, index) => {
+    title = titlesObj[index];
     html += `
     <div class="card my-3 mx-3 noteCard" id="noteCard" style="width: 18rem">
     <div class="card-body">
-      <h5 class="card-title">Note ${index + 1}</h5>
+      <h5 class="card-title">${title ? title : `Note: ${index}`}</h5>
       <p class="card-text">${element}</p>
       <button onclick="deleteNotes(this.id)" id=${index} class="btn btn-sm btn-danger">
         Delete
@@ -51,14 +65,20 @@ function showNotes() {
 
 // Function to delete the notes
 const deleteNotes = (index) => {
+  let titlesStr = localStorage.getItem("titles");
   let notesStr = localStorage.getItem("notes");
+  let titlesObj;
   let notesObj;
-  if (notesStr == null) {
+  if (notesStr == null && titlesStr == null) {
+    titlesObj = [];
     notesObj = [];
   } else {
+    titlesObj = JSON.parse(titlesStr);
     notesObj = JSON.parse(notesStr);
   }
   notesObj.splice(index, 1);
+  titlesObj.splice(index, 1);
+  localStorage.setItem("titles", JSON.stringify(titlesObj));
   localStorage.setItem("notes", JSON.stringify(notesObj));
   showNotes();
 };
@@ -75,12 +95,12 @@ search.addEventListener("input", () => {
     } else {
       element.style.display = "none";
     }
-  })
+  });
 });
 
 /*
 Further Features:
-1. Add Title
+1. Add Title | DONE
 2. Mark a note as Important
 3. Separate notes by user
 4. Sync and host to web server 
